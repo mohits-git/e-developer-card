261c74230e5d939dc388db5c719ee95615fea071
mohits-git <mohitkhatri2805@gmail.com>
2024-01-06 12:26:54 +0530

    Added vercel analytics

--- frontend-e-card/src/components/Layout.jsx
+++ frontend-e-card/src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Layout() {
     return (
         <>
+        <Analytics />
         <Header />
         <Outlet />
         <Footer />
